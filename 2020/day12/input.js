c60a8346f661abed549950bfbe8c7b2d10c4c72d
david <simplesam8856@gmail.com>
2021-08-24 09:59:43 +1000

    templates

---
+++ 2020/day12/input.js
@@ -0,0 +1,17 @@
+import fs from 'fs'
+
+const data = fs
+  .readFileSync('data.txt', 'utf8')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((row) => row.split(''))
+
+const testData = fs
+  .readFileSync('test_data.txt', 'utf8')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((row) => row.split(''))
+
+export { data, testData }
