c8922a4d064340e94f186feb36e6d95d0f039852
renn72 <simplesam8856@gmail.com>
2021-08-23 10:57:42 +1000

    day8/2

--- 2020/day8/day8.js
+++ 2020/day8/day8.js
@@ -43,8 +43,14 @@
 import fs from 'fs'
 
 const data = fs.readFileSync('day8.txt')
+const testData = fs.readFileSync('day8demo.txt')
 
 const instructions = data
+  .toString()
+  .split('\n')
+  .map((instruction) => instruction.split(' '))
+
+const testInstructions = testData
   .toString()
   .split('\n')
   .map((instruction) => instruction.split(' '))
@@ -58,6 +64,39 @@ const parseNumber = (num) => {
 }
 
 // console.log(parseNumber(instructions[1][1]))
+// After some careful analysis, you believe that exactly one instruction is corrupted.
+
+// Somewhere in the program, either a jmp is supposed to be a nop, or a nop is supposed to be a jmp. (No acc instructions were harmed in the corruption of this boot code.)
+
+// The program is supposed to terminate by attempting to execute an instruction immediately after the last instruction in the file. By changing exactly one jmp or nop, you can repair the boot code and make it terminate correctly.
+
+// For example, consider the same program from above:
+
+// nop +0
+// acc +1
+// jmp +4
+// acc +3
+// jmp -3
+// acc -99
+// acc +1
+// jmp -4
+// acc +6
+// If you change the first instruction from nop +0 to jmp +0, it would create a single-instruction infinite loop, never leaving that instruction. If you change almost any of the jmp instructions, the program will still eventually find another jmp instruction and loop forever.
+
+// However, if you change the second-to-last instruction (from jmp -4 to nop -4), the program terminates! The instructions are visited in this order:
+
+// nop +0  | 1
+// acc +1  | 2
+// jmp +4  | 3
+// acc +3  |
+// jmp -3  |
+// acc -99 |
+// acc +1  | 4
+// nop -4  | 5
+// acc +6  | 6
+// After the last instruction (acc +6), the program terminates by attempting to run the instruction below the last instruction in the file. With this change, after the program terminates, the accumulator contains the value 8 (acc +1, acc +1, acc +6).
+
+// Fix the program so that it terminates normally by changing exactly one jmp (to nop) or nop (to jmp). What is the value of the accumulator after the program terminates?
 
 const parseProgram = (instructions) => {
   console.log('started')
@@ -66,25 +105,70 @@ const parseProgram = (instructions) => {
   const notVisited = new Set([...Array(instructions.length).keys()])
   console.log(notVisited)
   while (true) {
+    console.log(index)
     if (!notVisited.has(index)) {
       return acc
     }
-
     console.log('started')
     notVisited.delete(index)
-
     let [instruction, num] = instructions[index]
-
     console.log([instruction, num])
-
     if (instruction === 'acc') {
       acc += parseNumber(num)
       index++
     } else if (instruction === 'jmp') {
       index += parseNumber(num)
-    }
+    } else [index++]
   }
 }
 
-console.log(instructions)
-console.log(parseProgram(instructions))
+const checkCode = (instructions, instruction, i) => {
+  const newInstruction = instruction === 'jmp' ? 'nop' : 'jmp'
+  let acc = 0
+  let index = 0
+  const notVisited = new Set([...Array(instructions.length).keys()])
+  while (true) {
+    if (!notVisited.has(index)) {
+      if (index > notVisited.size) {
+        return acc
+      } else {
+        return false
+      }
+    }
+    notVisited.delete(index)
+    let [instruction, num] = instructions[index]
+    if (i === index) {
+      // console.log([i, index, instruction, newInstruction])
+      instruction = newInstruction
+      // console.log(instruction)
+    }
+    if (instruction === 'acc') {
+      acc += parseNumber(num)
+      index++
+    } else if (instruction === 'jmp') {
+      index += parseNumber(num)
+    } else [index++]
+  }
+}
+
+const findFix = (instructions) => {
+  let acc = 0
+  instructions.forEach(([instruction, num], i) => {
+    console.log('index ' + i)
+    if (instruction === 'jmp' || instruction === 'nop') {
+      const res = checkCode(instructions, instruction, i)
+      if (res) {
+        console.log('res ' + res)
+        acc = res
+      }
+    }
+  })
+  return acc
+}
+
+// console.log(instructions.length)
+// console.log(instructions.slice(0, 100))
+// console.log(findFix(instructions))
+console.log(testInstructions)
+console.log(findFix(testInstructions))
+// console.log(parseProgram(instructions))
