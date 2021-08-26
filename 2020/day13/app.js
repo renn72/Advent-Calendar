b1bef294c078cfef5dbaae4557b4ad974ea85d89
renn72 <simplesam8856@gmail.com>
2021-08-27 08:14:54 +1000

    day13/1

--- 2020/day13/app.js
+++ 2020/day13/app.js
@@ -2,3 +2,24 @@ import { data, testData } from './input.js'
 
 console.log(data)
 console.log(testData)
+
+const findBus = (timetable) => {
+  const [time, _buses] = timetable
+  const buses = _buses.filter((bus) => bus != 'x')
+
+  return buses
+    .map((bus) => [bus, Math.ceil(time / bus) * bus - time])
+    .reduce((bestTime, idx, i) => {
+      if (i === 0) {
+        return idx
+      } else {
+        if (bestTime[1] > idx[1]) {
+          return idx
+        }
+      }
+      return bestTime
+    }, [])
+    .map((res) => [...res, +res[0] * res[1]])
+}
+
+console.log(findBus(testData))
