81b6fb7a7bf560664fd5eb00a7927c9d41d56027
renn72 <simplesam8856@gmail.com>
2021-08-27 13:24:06 +1000

    day13/2

--- 2020/day13/app.js
+++ 2020/day13/app.js
@@ -21,4 +21,55 @@ const findBus = (timetable) => {
     }, [])
 }
 
-console.log(findBus(data))
+const LCM = (pair1, pair2, interval, t) => {
+  const [t1, id1] = pair1
+  const [t2, id2] = pair2
+
+  let time = t + interval
+
+  while (true) {
+    // console.log(time)
+    if ((time + t1) % id1 === 0 && (time + t2) % id2 === 0) {
+      console.log('first match ' + time)
+      let mark = time
+      while (true) {
+        time += interval
+        if ((time + t1) % id1 === 0 && (time + t2) % id2 === 0) {
+          return [mark, time - mark]
+        }
+      }
+    }
+    time += interval
+  }
+}
+
+const timeTable = (times) => {
+  times = times
+    .map((time, i) => [i, time])
+    .filter((time) => time[1] !== 'x')
+    .map((time) => [time[0], +time[1]])
+
+  let index = 0
+  let interval = 1
+  let time = 0
+  let i = 0
+  while (true && index < times.length - 1) {
+    i++
+
+    let res = LCM(times[index], times[index + 1], interval, time)
+    time = res[0]
+    interval = res[1]
+    console.log(`time:${time} Int:${interval} res:${time + interval}`)
+    index++
+  }
+
+  return times
+}
+
+console.log(timeTable([17, 'x', 13, 19]))
+console.log(timeTable([67, 7, 59, 61]))
+console.log(timeTable([67, 'x', 7, 59, 61]))
+console.log(timeTable([67, 7, 'x', 59, 61]))
+console.log(timeTable([1789, 37, 47, 1889]))
+console.log(timeTable(testData[1]))
+console.log(timeTable(data[1]))
